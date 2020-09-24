a1e8f9d2491cacb2c9bd5050827db8ba86359fd7
ankit K <ankit48365@gmail.com>
2020-09-24 07:27:20 -0400

    9/24 Synch

---
+++ Colors_Sc_Blocks.jl
@@ -0,0 +1,47 @@
+### A Pluto.jl notebook ###
+# v0.11.14
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b0b36c60-fe57-11ea-31eb-bba7adb3c666
+
+
+# ╔═╡ ec9180f0-fe57-11ea-0197-63d4acf0e19f
+
+
+# ╔═╡ ec7bfd20-fe57-11ea-11ff-ad7d1063c690
+
+
+# ╔═╡ ec64cba0-fe57-11ea-0a43-296a76f4e41f
+
+
+# ╔═╡ ec4ef9b0-fe57-11ea-3218-19d3eb36b629
+
+
+# ╔═╡ ec39eb10-fe57-11ea-1a17-f5177a3a2bc4
+
+
+# ╔═╡ ec2578b0-fe57-11ea-21f9-3d06f046e6c7
+
+
+# ╔═╡ ec0dd1fe-fe57-11ea-2bff-8f9c00037c98
+
+
+# ╔═╡ ebe03252-fe57-11ea-3150-d3945169079b
+
+
+# ╔═╡ eb561abe-fe57-11ea-083b-7926bd86e4e9
+
+
+# ╔═╡ Cell order:
+# ╠═b0b36c60-fe57-11ea-31eb-bba7adb3c666
+# ╠═ec9180f0-fe57-11ea-0197-63d4acf0e19f
+# ╠═ec7bfd20-fe57-11ea-11ff-ad7d1063c690
+# ╠═ec64cba0-fe57-11ea-0a43-296a76f4e41f
+# ╠═ec4ef9b0-fe57-11ea-3218-19d3eb36b629
+# ╠═ec39eb10-fe57-11ea-1a17-f5177a3a2bc4
+# ╠═ec2578b0-fe57-11ea-21f9-3d06f046e6c7
+# ╠═ec0dd1fe-fe57-11ea-2bff-8f9c00037c98
+# ╠═ebe03252-fe57-11ea-3150-d3945169079b
+# ╠═eb561abe-fe57-11ea-083b-7926bd86e4e9
